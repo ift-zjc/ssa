79caf642508a3fbafb8c8c288b8224c1f20bfaea
Zhijiang Chen <zhijiang@chen.me>
2017-08-31 18:07:04 -0400

    Collision data parsing

--- src/main/resources/static/js/portal.js
+++ src/main/resources/static/js/portal.js
@@ -134,6 +134,12 @@ function handleTick(clock){
     // Got the index for P
     var p = (Number(satellitePair[0].collisionData[leftIndex]) + Number(satellitePair[0].collisionData[leftIndex+1]))/2;
     var p = p*100;
+
+    if(p>80){
+        $('.progress-bar').addClass('bg-danger');
+    }else(
+        $('.progress-bar').removeClass('bg-danger');
+    )
     $('.progress-bar').css('width', p.toFixed(2)+'%').attr('aria-valuenow', p.toFixed(2));
     $('.progress-bar').html(p.toFixed(2) + '%');
     console.log(p);
