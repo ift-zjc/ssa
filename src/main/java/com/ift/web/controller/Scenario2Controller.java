a6d7e0f708d27dfba978ee7ff431d231875e712d
Zhijiang Chen <zhijiang.chen@intfusiontech.com>
2017-11-02 10:48:08 -0400

    fix slow loading problem, link all html files with server and cesium container

---
+++ src/main/java/com/ift/web/controller/Scenario2Controller.java
@@ -0,0 +1,44 @@
+package com.ift.web.controller;
+
+import com.ift.domain.Satellite;
+import com.ift.services.SatelliteService;
+import com.ift.web.forms.SatelliteControlForm;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
+
+@Controller
+public class Scenario2Controller {
+    private static final Logger LOGGER = Logger.getLogger(PortalController.class.getName());
+
+    @Autowired
+    SatelliteService satelliteService;
+
+    @GetMapping(value = "/scenario2")
+    public String showPortal(Model model){
+        SatelliteControlForm satelliteControlForm = new SatelliteControlForm();
+        satelliteControlForm.setSatelliteList(satelliteService.listSatellites());
+        model.addAttribute("satellite", satelliteControlForm);
+        LOGGER.info("Start Portal Web Request");
+        return "Scenario2";
+    }
+
+
+    /**
+     * Model attributes
+     */
+
+    /**
+     * Satelites list
+     * @return
+     */
+    @ModelAttribute("allSatellites")
+    public List<Satellite> modelSatellites(){
+        return satelliteService.listSatellites();
+    }
+}
